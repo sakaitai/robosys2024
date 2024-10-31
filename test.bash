5334760c355ba1fc4e65d6491bd55f0a1bdec7e9
sakaitaisei <justdoit0430@i.softbank.jp>
2024-10-31 12:46:04 +0900

    plus

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,8 +6,19 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO" 
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+
+out=$(echo あ | ./plus)
+[ "${res}" = 0 ] && echo OK
+[ "${out}" = "" ] || ng "$LINENO"
+
+
+out=$(echo | ./plus)
+[ "$?" = 1]       || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "#res" = 0 ] && echo OK
 exit "$res"
