71bed26a5bc3d6d7f8df087ae46fcda5ce9f62c7
sakaitaisei <justdoit0430@i.softbank.jp>
2024-11-07 12:30:05 +0900

    test bash

--- test.bash
+++ test.bash
@@ -14,13 +14,13 @@ out=$(seq 5 | ./plus)
 
 
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-[ "#res" = 0 ] && echo OK
+[ "${res}" = 0 ] && echo OK
 exit $res
